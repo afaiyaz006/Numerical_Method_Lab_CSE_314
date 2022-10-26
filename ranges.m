7bfe1fc0e09c7609dc9fcf1f1fbde1974316c165
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-10-26 20:36:13 +0600

    added ranges practice code

---
+++ ranges.m
@@ -0,0 +1,10 @@
+A=[1,2,3;4,5,6;7,8,9]
+% start:step:stop %
+
+1:3:10
+% generate 1 to 10 1,2,3,4....10 gap is 1 %
+A=[1:1:10]
+
+% generate a to b with n elements %
+% linspace ( a,b,n) %
+B=linspace(2,4,100)

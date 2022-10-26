bdd91e376af9b4341890a9e803cb256140d61b53
Ahmed Faiyaz <69722755+afaiyaz006@users.noreply.github.com>
2022-10-26 23:16:46 +0600

    updated

--- bisect.m
+++ bisect.m
@@ -4,15 +4,15 @@ y=@(x) x.^3-0.165*x.^2+3.993*10.^-4
 x_l=0.1
 x_u=0.2
 
-x_m=(x_l+x_u)/2
+
 
 if y(x_u)*y(x_l)<0
   for i=1:5
+      x_m=(x_l+x_u)/2
       if y(x_l)*y(x_m)>0
         x_l=x_m;
       else
         x_u=x_m;
-      x_m=(x_l+x_u)/2
       end
   endfor
   disp(x_m)
